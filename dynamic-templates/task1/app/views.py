f7d3039e680c9e7c16c7304aee7323b51fc4ed5e
anvartdinovtimurlinux <39489844+anvartdinovtimurlinux@users.noreply.github.com>
2020-07-28 15:41:46 +0300

    Update views.py

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -7,7 +7,7 @@ def inflation_view(request):
 
     with open('./inflation_russia.csv', encoding='utf-8') as f:
         inflation = list(csv.reader(f, delimiter=';'))
-    print(inflation[1])
+
     context = {
         'table_header': inflation[0],
         'table_body': inflation[1:],
